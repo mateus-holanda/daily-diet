b8b0504ea47b556908bcf1f5d39e4860214fde7a
Mateus Holanda <mateus23ita@gmail.com>
2023-04-26 17:53:15 -0300

    First commit

---
+++ src/theme/index.ts
@@ -0,0 +1,33 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+
+    GRAY_700: '#FAFAFA',
+    GRAY_600: '#DDDEDF',
+    GRAY_500: '#DDDEDF',
+    GRAY_400: '#B9BBBC',
+    GRAY_300: '#5C6265',
+    GRAY_200: '#333638',
+    GRAY_100: '#1B1D1E',
+
+    GREEN: '#CBE4B4',
+    GREEN_LIGHT: '#E5F0DB',
+    GREEN_DARK: '#639339',
+
+    RED: '#F3BABD',
+    RED_LIGHT: '#F4E6E7',
+    RED_DARK: '#BF3B44'
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Nunito_400Regular',
+    BOLD: 'Nunito_700Bold'
+  },
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    XXL: 32
+  }
+};
