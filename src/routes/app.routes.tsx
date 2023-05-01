586966592659f50ea65a035e7dd32a7cdfd74c6f
Mateus Holanda <mateus23ita@gmail.com>
2023-05-01 05:22:21 -0300

    Add app's routes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,22 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Home } from '@screens/Home';
+import { NewMeal } from '@screens/NewMeal';
+import { MealInfo } from '@screens/MealInfo';
+import { EditMeal } from '@screens/EditMeal';
+import { MealRegistered } from '@screens/MealRegistered';
+
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false, animation: "fade_from_bottom" }}>
+      <Screen name="home" component={Home} />
+      <Screen name="new" component={NewMeal} />
+      <Screen name="info" component={MealInfo} />
+      <Screen name="edit" component={EditMeal} />
+      <Screen name="registered" component={MealRegistered} />
+    </Navigator>
+  );
+}
